1525158c17b5da85c9fd146752d8666366e9f055
Abdul Ahad <abdulahadansari810.com>
2023-11-29 12:42:37 +0530

    change everything

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brightColor: "#B8F4E8",
+        backgroundColor: "#E45365",
+      },
+    },
+  },
+  plugins: [],
+};
